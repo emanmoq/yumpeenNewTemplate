36190c1d3e6417421ccda79e086ac0ea6ab87ad8
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-11-02 23:13:45 +0200

    ddd

---
+++ assets/js/counter.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+
+    if($('.counterNum').length){
+        $('.counterNum').counterUp({
+          delay: 10,
+          time: 1000
+        });
+    }
+    
+});
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
 jQuery(function ($) {
 
+    $('.heroSlider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        items:1,
+        dots:true,
 
+    });
+    $('.MemberSlider').owlCarousel({
+        loop:false,
+        margin:20,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+            },
+            600:{
+                items:2,
+                nav:false,
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
 
 });
 
