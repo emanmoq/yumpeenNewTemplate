97abb77f13b1796574d8dec8abc4984b0ab50539
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-11-06 12:59:27 +0200

    fff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,52 @@
 jQuery(function ($) {
 
     $('.heroSlider').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        items:1,
-        dots:true,
-
+        loop: true,
+        margin: 10,
+        nav: false,
+        items: 1,
+        dots: true,
     });
     $('.MemberSlider').owlCarousel({
-        loop:false,
-        margin:20,
-        nav:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1,
-                nav:true,
+        loop: false,
+        margin: 20,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
             },
-            600:{
-                items:2,
-                nav:true,
+            600: {
+                items: 2,
+                nav: true,
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     });
     $('.testimonialSlider').owlCarousel({
-        loop:false,
-        margin:20,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1,
-                dots:false,
+        loop: false,
+        margin: 20,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: false,
             },
-            600:{
-                items:2,
-                dots:false,
+            600: {
+                items: 2,
+                dots: false,
             },
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
     });
-
+    $("#menuIcon").click(function () {
+        $("#sideMenu").toggle("slow");
+    });
 });
 
